c575612f2ba2f0a4d056d0fa7b9b72cd4c6c4d95
Fredyspt <fredyfps@gmail.com>
2021-03-23 11:27:40 -0700

    Added initial HTML and CheckD20 Function

---
+++ CheckD20.js
@@ -0,0 +1,46 @@
+function CheckD20(){
+    // Fetch current values
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const worksheet = spreadsheet.getSheetByName("Current Values");
+  
+    // (row, column, numRows, numColumns)
+    var dataRange = worksheet.getRange("B2:B8");
+    var data = dataRange.getValues();
+  
+    var sodaConcentration = data[0];
+    var viscosityNoe = data[1];
+    var solidsNoe = data[2];
+    var thicknessNoe = data[3];
+    var viscosityNoria = data[4];
+    var solidsNoria = data[5];
+    var thicknessNoria = data[6];
+  
+    var SodaOk = sodaConcentration > 9 || sodaConcentration < 11;
+    var vNoeOk = viscosityNoe > 2.1 || viscosityNoe < 3.5;
+    var sNoeOk = solidsNoe > 17 || solidsNoe < 23;
+    var tNoeOk = thicknessNoe > 0.7 || thicknessNoe < 0.9;
+    var vNoriaOk = viscosityNoria > 1.8 || viscosityNoria < 3.0;
+    var sNoriaOk = solidsNoria > 31 || solidsNoria < 37;
+    var tNoriaOk = thicknessNoria > 2.4 || thicknessNoria < 2.8;
+  
+    let chemicals = [SodaOk, vNoeOk, sNoeOk, tNoeOk, vNoriaOk, sNoriaOk, tNoriaOk];
+  
+    // If any value of the array is false, it returns false
+    var somethingWrong = !chemicals.every(Boolean);
+
+    if(somethingWrong){
+      var subject = 'D20 Parametros fuera de rango';
+      SendEmail(subject, message);
+    }
+  }
+
+  function createHtml(){
+      const htmlTemplate = HtmlService.createTemplateFromFile("emailD20");
+      htmlTemplate.sodaConcentration = sodaConcentration;
+      htmlTemplate.viscosityNoe = viscosityNoe;
+      htmlTemplate.solidsNoe = solidsNoe;
+      htmlTemplate.thicknessNoe = thicknessNoe;
+      htmlTemplate.viscosityNoria = viscosityNoria;
+      htmlTemplate.solidsNoria = solidsNoria;
+      htmlTemplate.thicknessNoria = thicknessNoria;
+  }
